7ee283882fee63dcc61c06b57663d6a96aac0461
Nolex01 <noel.szabo02@icloud.com>
2024-02-12 21:13:55 +0100

    refactor: Show logout href only to logged users

--- app/Views/dashboard.blade.php
+++ app/Views/dashboard.blade.php
@@ -2,13 +2,12 @@
 
 @if(isset($_SESSION['user_id']))
     <h1>Hello, {{ $_SESSION['email'] }} !</h1>
+    <nav>
+        <ul>
+            <li><a href="/logout">Logout</a></li>
+        </ul>
+    </nav>
 @else
     <p>Please log in to view this content.</p>
     <a href="/login">Login</a>
 @endif
-
-<nav>
-    <ul>
-        <li><a href="/logout">Logout</a></li>
-    </ul>
-</nav>
