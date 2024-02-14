94003ee4b69f6bb894dc2850f3b9d3004e9ff818
Anna Pomichal <rucsna@gmail.com>
2024-02-14 10:17:43 +0100

    feat: Add question form to dashboard.blade.php

--- app/Views/dashboard.blade.php
+++ app/Views/dashboard.blade.php
@@ -2,6 +2,14 @@
 
 @if(isset($_SESSION['user_id']))
     <h1>Hello, {{ $_SESSION['email'] }} !</h1>
+    <h2>Ask a question</h2>
+    <form method="post" action="/saveQuestion">
+        <label for="title">Title</label>
+        <input type="text" name="title" value=""><br/>
+        <label for="message">Message</label>
+        <input type="text" name="message" value=""><br/>
+        <button type="submit" name="submit" value="Submit">Save</button>
+    </form>
     <nav>
         <ul>
             <li><a href="/logout">Logout</a></li>
