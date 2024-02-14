3c1892e4befb78b16b7f988503252fd07d4b19b0
Nolex01 <noel.szabo02@icloud.com>
2024-02-14 18:10:19 +0100

    feat: Add vote route to index.php

--- public/index.php
+++ public/index.php
@@ -44,7 +44,8 @@ $routes = [
     '/login-action' => 'AuthController@login',
     '/logout' => 'AuthController@logout',
     '/dashboard' => 'DashboardController@index',
-    '/saveQuestion-action' => 'DashboardController@saveQuestion'
+    '/saveQuestion-action' => 'DashboardController@saveQuestion',
+    '/vote' => 'HomeController@vote'
 ];
 
 
