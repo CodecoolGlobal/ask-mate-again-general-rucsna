f07fd70f38ae07e18c66405eed62cbe71404a8a4
Nolex01 <noel.szabo02@icloud.com>
2024-02-14 18:12:19 +0100

    feat: Implement Voting into HomeController.php class

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,16 +4,20 @@ namespace App\Controllers;
 
 use App\Repositories\QuestionsRepository;
 use App\Repositories\RepositoryInterface;
+use App\Repositories\VoteRepository;
+
 use eftec\bladeone\BladeOne;
 
 class HomeController extends BaseController
 {
     private RepositoryInterface $repository;
+    private VoteRepository $voteRepository;
 
     public function __construct(BladeOne $blade)
     {
         parent::__construct($blade);
         $this->repository = new QuestionsRepository();
+        $this->voteRepository = new VoteRepository();
     }
 
     public function index(): void
@@ -23,6 +27,18 @@ class HomeController extends BaseController
             echo $this->blade->run('home', ['questions' => $questions]);
         } catch (\Exception $e) {
             echo "$e";
+        }
+    }
+
+    public function vote(): void
+    {
+        $vote = $_POST['vote'];
+        $questionId = $_POST['question_id'];
+
+        if($vote === 'up'){
+            $this->voteRepository->upvote($questionId);
+        } elseif ($vote === 'down'){
+            $this->voteRepository->downvote($questionId);
         }
     }
 }
