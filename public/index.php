e9921ce4af2642e68b5fba0c08b00b4778d71097
Anna Pomichal <rucsna@gmail.com>
2024-02-14 10:18:11 +0100

    feat: Add saveQuestion route to index.php

--- public/index.php
+++ public/index.php
@@ -44,6 +44,7 @@ $routes = [
     '/login-action' => 'AuthController@login',
     '/logout' => 'AuthController@logout',
     '/dashboard' => 'DashboardController@index',
+    '/saveQuestion-action' => 'DashboardController@saveQuestion'
 ];
 
 
