ca5b18e4b3b11ea9d25dfb451f527704427d43be
Anna Pomichal <rucsna@gmail.com>
2024-02-14 11:39:54 +0100

    feat: Create question.blade.php

---
+++ app/Views/question.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question Display Page</title>
+</head>
+<body>
+<h2>{{$question->title}}</h2>
+<p>{{$question->message}}</p>
+
+</body>
+</html>
