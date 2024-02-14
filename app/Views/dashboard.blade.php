08797e3288a831e28417ac2b0fe952e15fc71452
Anna Pomichal <rucsna@gmail.com>
2024-02-14 11:04:56 +0100

    refactor: Add image_id as hidden input

--- app/Views/dashboard.blade.php
+++ app/Views/dashboard.blade.php
@@ -3,12 +3,13 @@
 @if(isset($_SESSION['user_id']))
     <h1>Hello, {{ $_SESSION['email'] }} !</h1>
     <h2>Ask a question</h2>
-    <form method="post" action="/saveQuestion">
+    <form method="post" action="/saveQuestion-action">
         <label for="title">Title</label>
-        <input type="text" name="title" value=""><br/>
+        <input type="text" name="title" value="Title"><br/>
         <label for="message">Message</label>
-        <input type="text" name="message" value=""><br/>
-        <button type="submit" name="submit" value="Submit">Save</button>
+        <input type="text" name="message" value="Write a question"><br/>
+        <input type="hidden" name="image_id" value="1">
+        <button type="submit" name="submit">Save</button>
     </form>
     <nav>
         <ul>
