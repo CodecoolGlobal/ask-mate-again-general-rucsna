bae2456ac65a09bce07b9d7c44779382936875c4
Anna Pomichal <rucsna@gmail.com>
2024-02-15 14:42:46 +0100

    feat: Create answer repository

---
+++ app/Repositories/AnswerRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Database;
+
+class AnswerRepository implements RepositoryInterface
+{
+    private PDO $PDO;
+
+    public function __construct()
+    {
+        $this->PDO = Database::getInstance()->getConnection();
+    }
+
+    public function findAll(): array
+    {
+        $sql = 'SELECT * FROM answer';
+        $selectAll = $this->PDO->query($sql);
+
+        $answers = [];
+        while ($row = $selectAll->fetch(PDO::FETCH_ASSOC)){
+            $answers[] = $row;
+        }
+        return $answers;
+    }
+
+    public function find(int $id)
+    {
+        $sql = 'SELECT * FROM answer WHERE id = :id';
+        $selectAnswerById = $this->PDO->prepare($sql);
+        $selectAnswerById->execute(['id' => $id]);
+
+        return $selectAnswerById->fetch();
+    }
+
+    public function save($entity): int
+    {
+        $sql = 'INSERT INTO answer(id_question, id_registered_user, message) VALUES(:id_question, :id_registered_user, :message)';
+        $insertNewAnswer = $this->PDO->prepare($sql);
+        $insertNewAnswer->execute(['id_question' => $entity['question_id'], 'id_registered_user' => $entity['user_id'], 'message' => $entity['message']]);
+
+        return $this->PDO->lastInsertId();
+    }
+
+    public function update($entity): void
+    {
+        $sql = 'UPDATE answer SET message = :message WHERE id = :id';
+        $updateAnswer = $this->PDO->prepare($sql);
+        $updateAnswer->execute(['message' => $entity['message'], 'id' => $entity['id']]);
+    }
+
+    public function delete(int $id): void
+    {
+        $sql = 'DELETE FROM answer WHERE id = :id';
+        $deleteAnswer = $this->PDO->prepare($sql);
+        $deleteAnswer->execute(['id' => $id]);
+    }
+}
