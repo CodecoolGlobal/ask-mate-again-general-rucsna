2d9851a865f0c8b5a64f3340c8207ab280259dd5
Nolex01 <noel.szabo02@icloud.com>
2024-02-15 21:01:02 +0100

    feat: Add support search

--- app/Views/home.blade.php
+++ app/Views/home.blade.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -17,6 +17,11 @@
     </ul>
 </nav>
 
+<form method="post" action="/search">
+    <input type="text" name="query" placeholder="Enter your search query">
+    <button type="submit">Search</button>
+</form>
+
 <div>
     <h1>Questions</h1>
     <table>
@@ -24,26 +29,33 @@
             <th>Title</th>
             <th>Message</th>
             <th>Vote number</th>
+            <th>Image</th>
         </tr>
         @foreach($questions as $question)
             <tr>
                 <td>{{$question['title']}}</td>
                 <td>{{$question['message']}}</td>
                 <td>{{$question['vote_number']}}</td>
+                <td>
+                        <?php
+                        $imagePath = $question['directory'] . '/' . $question['file_name'];
+                        ?>
+                    <img src="<?php echo $imagePath; ?>" alt="Question Image" style="max-width: 100px;">
+                </td>
                 @if(isset($_SESSION['user_id']))
-                <td>
-                    <form action="/vote" method="post">
-                        <input type="hidden" name="id" value="{{$question['id']}}">
-                        <button type="submit" name="vote" value="up">Upvote</button>
-                        <button type="submit" name="vote" value="down">Downvote</button>
-                    </form>
-                </td>
-                <td>
-                    <form method="post" action="/answer">
-                        <input type="hidden" name="id" value="{{$question['id']}}">
-                        <input type="submit" name="update" value="Show">
-                    </form>
-                </td>
+                    <td>
+                        <form action="/vote" method="post">
+                            <input type="hidden" name="id" value="{{$question['id']}}">
+                            <button type="submit" name="vote" value="up">Upvote</button>
+                            <button type="submit" name="vote" value="down">Downvote</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form method="post" action="/answer">
+                            <input type="hidden" name="id" value="{{$question['id']}}">
+                            <input type="submit" name="update" value="Show">
+                        </form>
+                    </td>
                 @endif
             </tr>
         @endforeach
