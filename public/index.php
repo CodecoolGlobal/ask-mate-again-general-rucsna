16fa2fb166bb78e1ee3ac704524bd47d2acbec1c
Nolex01 <noel.szabo02@icloud.com>
2024-02-15 16:13:40 +0100

    refactor: Remove unused security features

--- public/index.php
+++ public/index.php
@@ -8,34 +8,9 @@ $viewsPath = dirname(__DIR__) . '/app/Views';
 $cachePath = dirname(__DIR__) . '/cache';
 $blade = new BladeOne($viewsPath, $cachePath);
 
-$allowedMethods = $config['allowedMethods'] ?? [];
-$allowedIPs = $config['allowedIPs'] ?? [];
-$allowedBrowsers = $config['allowedBrowsers'] ?? [];
-
 $uri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-/*
-if (array_key_exists($uri, $allowedMethods) && !in_array($requestMethod, $allowedMethods[$uri])) {
-    http_response_code(405);
-    echo '405 Method Not Allowed';
-    exit;
-}
 
-$clientIP = $_SERVER['REMOTE_ADDR'];
-if (!in_array($clientIP, $allowedIPs)) {
-    http_response_code(403);
-    echo '403 Forbidden';
-    exit;
-}
-
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-$browser = get_browser(null, true)['browser'];
-if (!in_array($browser, $allowedBrowsers)) {
-    http_response_code(403);
-    echo '403 Forbidden';
-    exit;
-}
-*/
 $routes = [
     '/' => 'HomeController@index',
     '/register' => 'AuthController@register',
